138e8f48a0e4d254b5785c3341035c7ccdbc76eb
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-10-10 09:26:19 -0700

    navbar is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import Main from './layouts/Main';
+import Topics from './Components/Topics/Topics';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Topics></Topics>
+        },
+      ]
+    },
+  ]);
+
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   );
 }
