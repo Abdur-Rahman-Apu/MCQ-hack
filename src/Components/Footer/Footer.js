15cb18e8509fd83a1e71e1624e243bac7cfa233c
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-10-10 18:45:21 -0700

    footer is added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-center my-5 fw-bold'>
+            Copyright &copy; MCQ hack
+        </div>
+    );
+};
+
+export default Footer;
