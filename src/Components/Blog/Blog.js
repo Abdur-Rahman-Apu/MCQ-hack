37815897618a236663385e3823bf7f0ffa6f32f8
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-10-12 07:58:32 -0700

    blog part design and responsive part are complete

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -4,7 +4,7 @@ import './Blog.css';
 const Blog = () => {
     return (
         <div>
-            <h3 className='text-center my-5'>Learn something new</h3>
+            <h3 className='text-center my-5 blog-title'>Learn something new</h3>
 
             <div className='blog-questions'>
                 <div className="accordion" id="accordionExample">
@@ -17,7 +17,7 @@ const Blog = () => {
                         <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
                                 <h6 className='ms-3'>For routing in React, React router is a standard library.</h6>
-                                <p className='fw-bold mb-1'>Features:</p>
+                                <p className='fw-bold mb-1 title ms-3'>Features:</p>
                                 <ul>
                                     <li>Enables the navigation among views of various components</li>
                                     <li>Allows to build a single page web application with navigation without the page refreshing </li>
@@ -35,7 +35,7 @@ const Blog = () => {
                         <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
                                 <p>The React Context API is a technique to make global variables and pass them effectively.</p>
-                                <span className='ms-4 fw-bold'>Working procedures:</span>
+                                <span className='ms-4 fw-bold title'>Working procedures:</span>
                                 <p className='mt-3'>At first, we have to use React.createContext() which will return a consumer and a provider. Provider is a component that allows consuming component to subscribe to context changes. It accepts a value which will be passed to consuming components that are descendants of this provider. One provider can be connected to many consumer. Consumer is a component that consumes and uses the state.</p>
                             </div>
                         </div>
@@ -47,12 +47,12 @@ const Blog = () => {
                             </button>
                         </h2>
                         <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                            <p className='mt-2'>The useRef hook accepts one argument as the initial value and returns a reference object which is mutable and it is known as current. The useRef hook can be used to store a mutable value that does not cause a re-render when updated</p>
-                            <span className='fw-bold ms-3'>Syntax</span>
-                            <p>const refContainer = useRef(initialValue);</p>
-                            <p>Firstly, we set the initial value: useRef(0). It is same as: </p>
+                            <p className='mt-2 ms-3'>The useRef hook accepts one argument as the initial value and returns a reference object which is mutable and it is known as current. The useRef hook can be used to store a mutable value that does not cause a re-render when updated</p>
+                            <span className='fw-bold ms-3 title'>Syntax</span>
+                            <p className='ms-3 '>const refContainer = useRef(initialValue);</p>
+                            <p className='ms-3 '>Firstly, we set the initial value: useRef(0). It is same as: </p>
                             <img className='img-fluid ms-3' src={'/useRef.png'} alt="useRef" />
-                            <p>We can access the count using count.current.</p>
+                            <p className='ms-3 mt-2'>We can access the count using count.current.</p>
                         </div>
                     </div>
                 </div>
